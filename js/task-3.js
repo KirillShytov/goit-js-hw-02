7bcdbd13bfeb240c904e18fabd2ca94e1070eab6
KirillShytov <sitovk587@gmail.com>
2023-11-04 16:43:48 +0200

    minus files

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,1 @@
-function calculateTotal(number) {
-  let result = 0;
-  let index = 0;
-  while (number >= index) {
-    result += index;
-    index++;
-    console.log('index=', index);
-    console.log('result=', result);
-  }
-  return result;
-}
-console.log(calculateTotal(3));
+
